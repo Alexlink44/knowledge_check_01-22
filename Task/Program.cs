83d851fcacf257ff63c78da4df533780b63e4004
Alex.link <alex.link_mail@e1.ru>
2022-02-02 13:23:34 +0500

    Version 2

--- Task/Program.cs
+++ Task/Program.cs
@@ -21,24 +21,23 @@ string PrintArray(string[] arr)
     {
         output += $"{arr[i]}, ";
     }
-    if (arr.Length == 0) {output = String.Empty;}
-    else {output = output.Substring(0, output.Length - 2);}
+    if (arr.Length != 0) {output = output.Substring(0, output.Length - 2);}
     return $"[{output}]";
 }
 
 string[] SelectArray(string[] array)
 {
-    string[] selectArr = new string[0];
+    string[] selectArr = new string[array.Length];
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length < 4)
         {
-            Array.Resize(ref selectArr, count + 1);
             selectArr[count] = array[i];
             count++;
         }
     }
+    Array.Resize(ref selectArr, count);
     return selectArr;
 }
 
